aaf0bf4f610a22b06bd877fd7b74472d44245254
Jotez <raf.cortez12@gmail.com>
2026-07-28 10:40:02 -0700

    Implemented server listening and added test.html to the repository

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import os
+from socket import *
+
+
+
 
 def main():
-    print("test")
+    serverPort = 12000
+    serverSocket = socket(AF_INET,SOCK_STREAM)
+    serverSocket.bind(("",serverPort))
+    serverSocket.listen(1)
+    print("server is listening")
+
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        message = connectionSocket.recv(1024).decode()
+        #decide how to deal with Http requests 
+        connectionSocket.close()
+
 
 main()
